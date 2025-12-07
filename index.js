8a186f44d0a4806ce419437da7a3237658de8753
Mohsin <104515551+mohsinsaleheen1@users.noreply.github.com>
2025-12-07 19:23:17 +0500

    Changes

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const userRoute = require("./routes/user.route.js");
-// const dashboardRoute = require("./routes/dashboard.route.js");
-// const jobsRoute = require("./routes/job.routes.js");
-// const uploadresume = require("./routes/resume.route.js");
-// const auth = require("./middleware/auth.js");
-// const connectDB = require("./config/db.js");
-// const path = require("path");
-// const cors = require("cors");
-// dotenv.config();
-// // Databse Connect
-// connectDB();
-// const app = express();
-// app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.json());
-// app.use(cors());
-// app.use(express.static("public"));
-// app.use(express.urlencoded({ extended: true }));
-// app.use("/api/users", userRoute);
-// app.use("/api/dashboard", auth, dashboardRoute);
-// app.use("/api/jobs", auth, jobsRoute);
-// app.use("/api/uploadresume", auth, uploadresume);
-// const PORT = process.env.PORT;
-// app.listen(PORT, () => {
-//   console.log(`Server is runing at http://localhost:${PORT}`);
-// });
-
 const express = require("express");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user.route.js");
@@ -36,33 +8,20 @@ const auth = require("./middleware/auth.js");
 const connectDB = require("./config/db.js");
 const path = require("path");
 const cors = require("cors");
-
 dotenv.config();
-
+// Databse Connect
 connectDB();
-
 const app = express();
-
-// ADD THIS FIX
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src * 'self' 'unsafe-inline' 'unsafe-eval' blob: data:;"
-  );
-  next();
-});
-
-app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api/users", userRoute);
 app.use("/api/dashboard", auth, dashboardRoute);
 app.use("/api/jobs", auth, jobsRoute);
 app.use("/api/uploadresume", auth, uploadresume);
-
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is runing at http://localhost:${PORT}`);
 });
