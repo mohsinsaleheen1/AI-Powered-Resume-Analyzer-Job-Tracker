520b12bcd753e6b36f8aef1132404d151993ea50
Mohsin <104515551+mohsinsaleheen1@users.noreply.github.com>
2025-12-09 22:28:06 +0500

    Port change

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.get("/", (req, res) => {
 });
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server is runing at http://localhost:${PORT}`);
+  console.log(`Server is runing at ${PORT}`);
 });
